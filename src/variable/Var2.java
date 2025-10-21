0442f277dafc56b0a8d502771391693b0725cb32
도미닉 <kiljh@me.com>
2025-10-21 10:18:27 +0900

    변수 값 변경

--- src/variable/Var2.java
+++ src/variable/Var2.java
@@ -3,10 +3,9 @@ package variable;
 public class Var2 {
     public static void main(String[] args) {
         int a;
-        a = 20;
-
+        a = 10;
         System.out.println(a);
-        System.out.println(a);
+        a = 50;
         System.out.println(a);
     }
 }
