25afbcd1cfa62ae1b6f57bb351e90942d1971c01
도미닉 <kiljh@me.com>
2025-10-21 10:28:49 +0900

    변수 선언

--- src/variable/Var2.java
+++ src/variable/Var2.java
@@ -3,9 +3,14 @@ package variable;
 public class Var2 {
     public static void main(String[] args) {
         int a;
-        a = 10;
+        a = 1;
         System.out.println(a);
-        a = 50;
-        System.out.println(a);
+
+        int b = 2;
+        System.out.println(b);
+
+        int c = 3, d = 4;
+        System.out.println(c);
+        System.out.println(d);
     }
 }
