5ece6ce33ae317f3c12bc0f7bf061cbec53c73f2
도미닉 <kiljh@me.com>
2025-11-30 16:48:42 +0900

    31. 문제와 풀이1

---
+++ src/condition/Test1.java
@@ -0,0 +1,28 @@
+package condition;
+
+public class Test1 {
+    public static void main(String[] args) {
+        int score = 85;
+        String degree;
+
+        switch(score/10) {
+            case 10:
+            case 9:
+                degree = "A";
+                break;
+            case 8:
+                degree = "B";
+                break;
+            case 7:
+                degree = "C";
+                break;
+            case 6:
+                degree = "D";
+                break;
+            default:
+                degree = "F";
+        }
+
+        System.out.println("학점은 " + degree + "입니다.");
+    }
+}
---
+++ src/condition/Test2.java
@@ -0,0 +1,20 @@
+package condition;
+
+public class Test2 {
+    public static void main(String[] args) {
+        Double distance = 100.1;
+        String vehicle;
+
+        if (distance <= 1) {
+            vehicle = "도보";
+        } else if (distance <= 10) {
+            vehicle = "자전거";
+        } else if (distance <= 100) {
+            vehicle = "자동차";
+        } else {
+            vehicle = "비행기";
+        }
+
+        System.out.println(vehicle + "를 이용하세요.");
+    }
+}
