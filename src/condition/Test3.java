37c849365fe6a97c070986018fab360df2877a13
도미닉 <kiljh@me.com>
2025-12-02 23:53:45 +0900

    31. 문제와 풀이1(3번 문제)

---
+++ src/condition/Test3.java
@@ -0,0 +1,18 @@
+package condition;
+
+public class Test3 {
+    public static void main(String[] args) {
+        int dollar = 10;
+        int exchangeRate = 1300;
+
+        if (dollar < 0) {
+            System.out.println("잘못된 금액입니다.");
+            return;
+        } else if (dollar == 0) {
+            System.out.println("환전할 금액이 없습니다.");
+            return;
+        }
+        int won = dollar * exchangeRate;
+        System.out.println("환전 금액은 " + won + "원입니다.");
+    }
+}
