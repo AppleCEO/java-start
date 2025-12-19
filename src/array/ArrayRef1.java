d0ce7daf5d325d5aa792c708874ed91cc90aca5b
도미닉 <kiljh@me.com>
2025-12-19 23:58:13 +0900

    62. 배열 리펙토링

--- src/array/ArrayRef1.java
+++ src/array/ArrayRef1.java
@@ -3,13 +3,7 @@ package array;
 public class ArrayRef1 {
     public static void main(String[] args) {
         int[] students;
-        students = new int[5];
-
-        students[0] = 90;
-        students[1] = 80;
-        students[2] = 70;
-        students[3] = 60;
-        students[4] = 50;
+        students = new int[]{90, 80, 70, 60, 50};
 
         for (int i = 0; i < students.length; i++) {
             System.out.println("학생" + (i+1) + " 점수: " + students[i]);
